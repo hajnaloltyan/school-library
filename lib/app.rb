b5c8f8aa9c48db03ce92e346ccd2f1348c0fec1a
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-10-07 00:42:11 +0200

    Add the function to list people

--- lib/app.rb
+++ lib/app.rb
@@ -8,5 +8,12 @@ class App
   def list_books
     @books.each do |book|
       p "Title: #{book.title}, Author: #{book.author}"
+    end
+  end
+
+  def list_people
+    @people.each do |person|
+      p "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
   end
 end
